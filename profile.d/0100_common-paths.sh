8a0896317a3fed167fb2c955332aab5164ab45f3
Alex Ameen <alex.ameen.tx@gmail.com>
2023-06-28 17:41:06 -0500

    ld-floxlib is segfaulting...

--- profile.d/0100_common-paths.sh
+++ profile.d/0100_common-paths.sh
@@ -28,18 +28,17 @@ export             \
   XDG_DATA_DIRS    \
 ;
 
-case "$(uname -s)" in
-Linux*)
-  if [ -z "${FLOX_NOSET_LD_AUDIT:-}" -a -e "$FLOX_ENV/lib/ld-floxlib.so" ]; then
+if [ -z "${FLOX_NOSET_LD_AUDIT:-}" ]; then
+  if [ -e "$FLOX_ENV/lib/ld-floxlib.so" ]; then
     LD_AUDIT="$FLOX_ENV/lib/ld-floxlib.so";
     export LD_AUDIT;
+  else  # Assume Darwin
+    : "${DYLD_FALLBACK_LIBRARY_PATH:=/usr/local/lib:/usr/lib}";
+    DYLD_FALLBACK_LIBRARY_PATH="$FLOX_ENV/lib:$DYLD_FALLBACK_LIBRARY_PATH";
+    export DYLD_FALLBACK_LIBRARY_PATH;
   fi
-  ;;
-Darwin*)
-  DYLD_FALLBACK_LIBRARY_PATH="$FLOX_ENV/lib:${DYLD_FALLBACK_LIBRARY_PATH:-/usr/local/lib:/usr/lib}";
-  export DYLD_FALLBACK_LIBRARY_PATH;
-  ;;
-esac
+fi
+
 
 # ---------------------------------------------------------------------------- #
 #
