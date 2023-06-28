baf794a72786e8cc2d9fa4994814ad1748788ea7
Alex Ameen <alex.ameen.tx@gmail.com>
2023-06-28 12:46:33 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Installable /etc/profile.d activation scripts for use with flox";
 
   inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
-  inputs.ld-floxlib.url = "github.com:flox/ld-floxlib";
+  inputs.ld-floxlib.url = "github:flox/ld-floxlib";
 
   outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (_: {});
 }
