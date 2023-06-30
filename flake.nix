793243f6b1771ac90ee9c8280e2e86b5975c9e8f
Alex Ameen <alex.ameen.tx@gmail.com>
2023-06-30 13:04:51 -0500

    use other branch for ld-floxlib

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Installable /etc/profile.d activation scripts for use with flox";
 
   inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
-  inputs.ld-floxlib.url = "github:flox/ld-floxlib";
+  inputs.ld-floxlib.url = "github:flox/ld-floxlib/aameen.skip-version";
 
   outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (_: {});
 }
