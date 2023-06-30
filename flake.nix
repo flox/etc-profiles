b299b1a81a462372f318bffadedab87f7d11d0cd
Alex Ameen <alex.ameen.tx@gmail.com>
2023-06-30 13:47:23 -0500

    kill capacitor with fire

--- flake.nix
+++ flake.nix
@@ -1,8 +1,80 @@
-{
-  description = "Installable /etc/profile.d activation scripts for use with flox";
+# ============================================================================ #
+#
+#
+#
+# ---------------------------------------------------------------------------- #
 
-  inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
+{
+
+# ---------------------------------------------------------------------------- #
+
+  description =
+    "Installable /etc/profile.d activation scripts for use with flox";
+
   inputs.ld-floxlib.url = "github:flox/ld-floxlib/aameen.skip-version";
 
-  outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (_: {});
+
+# ---------------------------------------------------------------------------- #
+
+  outputs = { nixpkgs, ld-floxlib, ... }: let
+
+# ---------------------------------------------------------------------------- #
+
+    eachDefaultSystemMap = let
+      defaultSystems = [
+        "x86_64-linux"  "aarch64-linux"  "i686-linux"
+        "x86_64-darwin" "aarch64-darwin"
+      ];
+    in fn: let
+      proc = system: { name = system; value = fn system; };
+    in builtins.listToAttrs ( map proc defaultSystems );
+
+
+# ---------------------------------------------------------------------------- #
+
+    overlays.deps = final: prev: {
+      ldFloxlib =
+        ( builtins.getAttr final.system ld-floxlib.packages ).ld-floxlib;
+    };
+    overlays.etc-profiles = final: prev: {
+      etc-profiles = final.callPackage ./pkgs/etc-profiles {
+        self = builtins.path { path = ./.; };
+      };
+    };
+    overlays.default =
+      nixpkgs.lib.composeExtension overlays.deps overlays.etc-profiles;
+
+
+# ---------------------------------------------------------------------------- #
+
+    legacyPackages = eachDefaultSystemMap ( system: let
+      nixpkgsFor = builtins.getAttr system nixpkgs.legacyPackages;
+      pkgsFor    = nixpkgsFor.extend overlays.default;
+    in {
+      inherit (pkgsFor) etcProfiles mkEtcProfile;
+    } );
+
+
+# ---------------------------------------------------------------------------- #
+
+  in {
+    inherit overlays legacyPackages;
+    packages = eachDefaultSystemMap ( system: let
+      profiles = ( builtins.getAttr system legacyPackages ).etcProfiles;
+      rename   = name: value: { name = "profile-" + name; inherit value; };
+    in builtins.listToAttrs ( builtins.attrValues (
+      builtins.mapAttrs rename profiles
+    ) ) );
+  };
+
+
+# ---------------------------------------------------------------------------- #
+
 }
+
+
+# ---------------------------------------------------------------------------- #
+#
+#
+#
+# ============================================================================ #
---
+++ pkgs/etc-profiles/pkg-fun.nix
@@ -0,0 +1,114 @@
+# ============================================================================ #
+#
+# Creates a starter `<env>/etc/profile' that aggregates
+# `<env>/etc/profile.d/*.sh' "child scripts".
+#
+# ---------------------------------------------------------------------------- #
+
+{ src
+, lib
+, ldFloxlib
+, config
+, bash
+, coreutils
+, hostPlatform
+, system
+}: let
+
+# ---------------------------------------------------------------------------- #
+
+  pname     = "etc-profiles";
+  version   = "0.1.0";
+  drv       = derivation {
+    inherit pname version system ldFloxlib;
+    name      = pname + "-" + version;
+    builder   = bash.outPath + "/bin/bash";
+    outputs   = ["common_paths" "node" "python3" "out"];
+    profile   = builtins.path { path = ( toString self ) + "/profile";   };
+    profile_d = builtins.path { path = ( toString self ) + "/profile.d"; };
+    PATH      = coreutils.outPath + "/bin";
+    body      = ''
+      mkdir -p "$out/etc"                     \
+               "$out/lib"                     \
+               "$common_paths/etc/profile.d"  \
+               "$node/etc/profile.d"          \
+               "$python3/etc/profile.d"       \
+      ;
+      cp -- "$profile" "$out/etc/profile";
+
+      ${if ! hostPlatform.isLinux then "" else ''
+          ln -s -- "$ldFloxlib/lib/"* "$out/lib/";
+        ''
+       }
+
+      cp -- "$profile_d/0100_common-paths.sh" "$common_paths/etc/profile.d/";
+      cp -- "$profile_d/0500_node.sh"         "$node/etc/profile.d/";
+      cp -- "$profile_d/0500_python3.sh"      "$python3/etc/profile.d/";
+    '';
+    passAsFile = "body";
+    args       = ["-eu" "-o" "pipefail" "-c" ". $bodyPath;"];
+  };
+
+
+# --------------------------------------------------------------------------- #
+
+in drv // {
+  meta = let
+    lfm     = ldFloxlib.meta or {};
+    license = lib.licenses.mit;
+    # Inherit broken from `ld-floxlib'
+    broken    = if hostPlatform.isLinux then ( lfm.broken or false ) else false;
+    platforms = [
+      "x86_64-linux"  "aarch64-linux"  "i686-linux"
+      "x86_64-darwin" "aarch64-darwin"
+    ];
+    unsupported = ! ( builtins.elem hostPlatform.system platforms );
+    unfree      = ! license.free;
+  in {
+    inherit (drv) name;
+    inherit license broken platforms unfree unsupported;
+    available = ( config.allowBroken            || ( ! broken      ) ) &&
+                ( config.allowUnfree            || ( ! unfree      ) ) &&
+                ( config.allowUnsupportedSystem || ( ! unsupported ) );
+    homepage         = "https://github.com/flox/etc-profiles";
+    outputsToInstall = ["common_paths" "python3" "node" "out"];
+    description      = ''
+      Installable /etc/profile.d activation scripts for use with flox
+    '';
+    longDescription = ''
+      An `<env>/etc/profile' script to source `<env>/etc/profile.d/*.sh`
+
+      Users can define and install scripts in `<env>/etc/profile.d' as
+      "custom packages"/installables to share common setup processes
+      across environments.
+
+      Recommended usage:
+        # flox.nix
+        {
+          packages.nixpkgs-flox.sqlite = {
+            meta.outputsToInstall = ["bin" "out" "dev"];
+          };
+
+          # Provides developer environment hooks for use with python3.
+          packages.flox.etc-profiles = {
+            # Optionally, specify language packages to install.
+            # Please note that all/most language packs depend on including
+            # the "common_paths" output, and ALL depend on "out".
+            meta.outputsToInstall = ["out" "common_paths" "python3"];
+          };
+
+          shell.hook = '${""}'
+            [[ -r "$FLOX_ENV/etc/profile" ]] && . "$FLOX_ENV/etc/profile";
+            pkg-config --list-all >&2;
+          '${""}'
+        }
+    '';
+  };
+}
+
+
+# ---------------------------------------------------------------------------- #
+#
+#
+#
+# ============================================================================ #
